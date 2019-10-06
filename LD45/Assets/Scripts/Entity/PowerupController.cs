d01c4ec29e119c47369bf8d355a02616263a92ba
RahmKota3 <38017498+RahmKota3@users.noreply.github.com>
2019-10-06 17:34:33 +0200

    Rocket

--- LD45/Assets/Scripts/Entity/PowerupController.cs
+++ LD45/Assets/Scripts/Entity/PowerupController.cs
@@ -8,6 +8,14 @@ public class PowerupController : MonoBehaviour
 {
     Powerups currentPowerup = Powerups.None;
 
+    [SerializeField] GameObject minePrefab;
+    [SerializeField] GameObject boostObject;
+    [SerializeField] GameObject rocketPrefab;
+    [SerializeField] GameObject shieldObject;
+
+    [SerializeField] Transform mineLaunchPoint;
+    [SerializeField] Transform rocketLaunchPoint;
+
     public void RandomizePowerup()
     {
         currentPowerup = (Powerups)Random.Range(1, 5);
@@ -18,5 +26,21 @@ public class PowerupController : MonoBehaviour
     {
         if (currentPowerup == Powerups.None)
             return;
+
+        switch (currentPowerup)
+        {
+            case Powerups.Mine:
+                Instantiate(minePrefab, mineLaunchPoint.position, transform.rotation);
+                break;
+            case Powerups.Boost:
+                boostObject.SetActive(true);
+                break;
+            case Powerups.Bazooka:
+                Instantiate(rocketPrefab, rocketLaunchPoint.position, transform.rotation);
+                break;
+            case Powerups.Shield:
+                shieldObject.SetActive(true);
+                break;
+        }
     }
 }
---
+++ LD45/Assets/Scripts/Powerups/Rocket.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float velocity = 30;
+    [SerializeField] float rotationSpeed = 0.5f;
+
+    Transform target;
+    bool lostTarget = false;
+
+    float angle;
+
+    Rigidbody rb;
+
+    void FlyTowardsTarget()
+    {
+        if (target == null)
+            return;
+
+        if (lostTarget == false)
+        {
+            Vector3 targetVector = (target.position - transform.position).normalized;
+
+            transform.forward = Vector3.Slerp(transform.forward, targetVector, rotationSpeed / 100);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+
+        rb.velocity = transform.forward * velocity;
+
+        angle = Vector3.Angle(target.transform.forward, (target.transform.position - transform.position).normalized);
+        if (angle > 80 && angle <= 90)
+            lostTarget = true;
+    }
+
+    void FindTarget()
+    {
+        EntityStats[] possibleTargets = FindObjectsOfType<EntityStats>();
+        float shortestDistance = Mathf.Infinity;
+        float distance = 0;
+
+        foreach (EntityStats es in possibleTargets)
+        {
+            if (this.transform == es.transform)
+                continue;
+            distance = Vector3.Distance(transform.position, es.transform.position);
+            if(distance <= shortestDistance)
+            {
+                angle = Vector3.Angle(transform.forward, (es.transform.position - transform.position).normalized);
+                if (angle >= 0 && angle < 45)
+                {
+                    shortestDistance = distance;
+                    target = es.transform;
+                }
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        FindTarget();
+
+        rb = GetComponent<Rigidbody>();
+        
+        rb.velocity = transform.forward * velocity;
+    }
+
+    private void FixedUpdate()
+    {
+        FlyTowardsTarget();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<EntityStats>() != null)
+        {
+
+        }
+        
+        Destroy(gameObject);
+    }
+}
