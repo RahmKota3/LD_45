47a559c6cc1d9d0b67183da4aabc14c518243700
RahmKota3 <38017498+RahmKota3@users.noreply.github.com>
2019-10-07 20:41:05 +0200

    music fix

---
+++ LD45/Assets/Scripts/MusicSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject musicPlayerPrefab;
+
+    private void Awake()
+    {
+        if (FindObjectOfType<MusicPlayer>() == null)
+            Instantiate(musicPlayerPrefab, Vector3.zero, Quaternion.identity);
+    }
+}
